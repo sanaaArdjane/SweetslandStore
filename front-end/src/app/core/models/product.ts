be5dc55ca0b40b9aaef711329542b47f85271811
sanaaArdjane <sanaaard@gmail.com>
2024-08-10 00:07:15 +0100

    updated productService

---
+++ front-end/src/app/core/models/product.ts
@@ -0,0 +1,6 @@
+export interface product{
+  id:number
+  name:string
+  category:string
+  description:string
+}
--- front-end/src/app/core/services/product.service.ts
+++ front-end/src/app/core/services/product.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { product } from '../models/product';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private url:string= "assets/product.json"
+productList:product[]=[];
+newProduct:product
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+//reading and retrieving the products from json file using httpCLient
+getProducts(){
+  return this.http.get(this.url).subscribe((response)=>{
+      this.productList = (response as any[]).map(product => ({
+        id: product.id,
+        name: product.name,
+        category: product.category,
+        description: product.description
+      }));
+
+    });
+  }
+
+
+  //adding new product
+  addProduct(product:product){
+    this.productList.push(product)
+  }
+  //removing product by id
+  removeProduct(id:number){
+    this.productList = this.productList.filter(product => product.id !== id);
+  }
+  //filter a list of products par categories
+  filterProduct(category:string){
+    this.productList=this.productList.filter(product => product.category == category);
+  }
 }
