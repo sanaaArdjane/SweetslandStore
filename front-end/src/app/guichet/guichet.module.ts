dc8873832554c0fd7c28ba7de9671c2cd4fb6077
sanaaArdjane <sanaaard@gmail.com>
2024-08-09 13:05:38 +0100

    Re-add front-end as a regular directory

---
+++ front-end/src/app/guichet/guichet.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GuichetRoutingModule } from './guichet-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GuichetRoutingModule
+  ]
+})
+export class GuichetModule { }
