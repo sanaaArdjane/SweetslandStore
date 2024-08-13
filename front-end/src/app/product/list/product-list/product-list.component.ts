faaca415f60d8a9420a87ac1550ae10cafc4a4db
sanaaArdjane <sanaaard@gmail.com>
2024-08-13 10:03:10 +0100

    updated the product-list component to subscribe to productService function

--- front-end/src/app/product/list/product-list/product-list.component.ts
+++ front-end/src/app/product/list/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../../../core/services/product.service';
+import { product } from '../../../core/models/product';
 
 @Component({
   selector: 'app-product-list',
@@ -9,8 +10,13 @@ import { ProductService } from '../../../core/services/product.service';
   styleUrl: './product-list.component.scss'
 })
 export class ProductListComponent {
+  products: product[] = [];
+  //inject the productService
 constructor(private productService:ProductService){}
+  //display the productList
 displayProductList(){
-  return this.productService.getProducts()
+  return this.productService.getProducts().subscribe({next:(products)=>{
+    this.products = products;
+  }})
 }
 }
